c8d374191a9f7011f4b57440026864b50ad4f15e
wormjim <gcooled29@gmail.com>
2019-07-27 10:36:50 -0400

    Babel & Webpack Init Setup

---
+++ babel.config.js
@@ -0,0 +1,46 @@
+module.exports = (api) => {
+  api.cache.forever();
+
+  const common = {
+    presets: [
+      '@babel/react',
+      [
+        '@babel/typescript',
+        {
+          isTSX: true,
+          allExtensions: true
+        }
+      ]
+    ],
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-object-rest-spread',
+      '@babel/plugin-syntax-dynamic-import',
+      [
+        'module-resolver',
+        {
+          extensions: ['.ts', '.tsx'],
+          root: ['./']
+        }
+      ]
+    ]
+  };
+
+  const server = {};
+  const frontend = {};
+
+  return {
+    plugins: [...common.plugins],
+    presets: [
+      [
+        '@babel/env',
+        {
+          modules: false,
+          targets: '> 0.25%, not dead',
+          useBuiltIns: 'entry'
+        }
+      ],
+      ...common.presets
+    ]
+  };
+};
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './src/pages/index.html',
+  title: 'Read Robin',
+  filename: 'index.html'
+});
+
+module.exports = (env) => {
+  const common = {
+    mode: env.production === 'production' ? env : 'development',
+    entry: {
+      source: './src/index.tsx'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/'
+    },
+    resolve: {
+      alias: {
+        config: path.resolve(__dirname, 'config/config')
+      },
+      extensions: ['.tsx', '.ts', '.js', '.json']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'babel.config.js')
+            }
+          }
+        }
+      ]
+    },
+    plugins: [htmlPlugin, new CleanWebpackPlugin()]
+  };
+
+  const production = {};
+  const development = {
+    devtool: 'inline-source-map',
+    devServer: {
+      compress: true,
+      contentBase: './dist',
+      noInfo: true,
+      open: true,
+      port: 9000,
+      watchContentBase: true
+    },
+    watchOptions: {
+      aggregateTimeout: 300,
+      ignored: /node_modules/
+    }
+  };
+
+  return {
+    ...common,
+    ...(env.production === 'production' ? production : development)
+  };
+};
