cdfa5c5aca8e2a41aa09df3abb6a780725ae5161
wormjim <gcooled29@gmail.com>
2019-08-28 22:05:39 -0400

    Construct Test Server File

---
+++ server/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const server = express();
+
+// eslint-disable-next-line no-magic-numbers
+server.listen(5000, () => {
+  // eslint-disable-next-line no-console
+  console.log('listening on port 5000');
+});
+
+server.get('/', (req, res) => {
+  res.send('Awesome! We\'re live debugging this!');
+});
